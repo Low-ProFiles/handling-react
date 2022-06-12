fe2e8ae52ab033896ef140379a831f58f88144a2
Low-profile <maxandy@naver.com>
2022-06-12 20:08:43 +0900

    feat: useState part

--- src/UseState/UseInput.js
+++ src/UseState/UseInput.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
-function UseInput(){
-    return(
-        <>
-            UseInput
-        </>
-    );
-};
+function UseInput() {
+  const useInput = (initialValue, validator) => {
+    const [value, setValue] = useState(initialValue);
+    const onChange = (e) => {
+      const {
+        target: { value }
+      } = e;
 
-export default UseInput
+      let willUpdate = true;
+      if (typeof validator === "function") {
+        //validator의 타입이 function이라면
+        //willUpdate에 validator 값 적용
+        willUpdate = validator(value);
+      }
+      if (willUpdate) {
+        setValue(value);
+      }
+    };
+    return { value, onChange };
+  };
+
+  const maxLen = (value) => value.length <= 10;
+  //validator를 maxLen으로 지정, 그 결과는 true or false
+  const name = useInput("멋진 ", maxLen);
+
+  return (
+    <div> 
+      <h1>Hello</h1>
+      <input placeholder="Name" {...name} />
+    </div>
+  );
+}
+
+export default UseInput;
